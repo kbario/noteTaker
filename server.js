b5afd1c8cc35e6eb5e237a23fcdcaf89995a68ae
kbario <kylebario1@gmail.com>
2022-02-20 07:36:17 +0800

    read and write file works in post

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const { v4 } = require("uuid");
-const req = require('express/lib/request');
+// const req = require('express/lib/request');
 
 // initialise express app
 const app = express();
@@ -13,24 +13,48 @@ const PORT = 3001
 
 // define middleware
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // get the html files and display them
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-})
 
 app.get('/notes', (req, res) => {
     res.sendFile(__dirname + '/public/notes.html')
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
 // notes
 app.get('/api/notes', (req, res) => {
-    res.json(__dirname + './db/db.json')
-})
+    const dbs = JSON.parse(fs.readFileSync(__dirname + '/db/db.json', 'utf-8'));
+    const response = {
+        status: 'success',
+        body: dbs
+    };
+    console.log(response);
+    res.status(200).json(response);
+});
 
 app.post('/api/notes', (req, res) => {
-    console.log(req.body)
-})
+    // get the new note sent in body
+    const newNote = req.body
+    // add a uuid to the new note
+    newNote.id = v4()
+    // get the existing notes
+    const dbs = JSON.parse(fs.readFileSync(__dirname + '/db/db.json', 'utf-8'))
+    // add the new note to existing
+    dbs.push(newNote)
+    // save new stuff
+    fs.writeFileSync(__dirname + '/db/db.json', JSON.stringify(dbs))
+    // return response
+
+    const response = {
+        status: 'success',
+        body: newNote,
+      };
+    res.status(201).json(response);
+});
 
 
 app.listen(PORT, () =>
